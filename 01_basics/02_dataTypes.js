1c0945d4e873f84043bf0ae244fde98b2213042b
tanish-ashu <ashu2005rajak@gmail.com>
2025-06-16 17:57:02 +0000

    datatypes

---
+++ 01_basics/02_dataTypes.js
@@ -0,0 +1,15 @@
+"use strict"; // treat all JS code as newer version (all new syntax are used , read according to that)
+
+// alert(6+6)  // we are using nodejs, not browser
+
+console.log( 3 + 3 );
+console.log("Hitesh");
+
+
+// null => standalone value
+// undefined => 
+// symbol => unique
+// number , bigint, string, boolean, symbol, object
+
+console.log(typeof(undefined)); // undefined
+console.log(typeof(null));    // object
