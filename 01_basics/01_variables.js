b9f33f74b6ddfc99cd5bf2378fab8908ac0d726b
tanish-ashu <ashu2005rajak@gmail.com>
2025-06-16 17:57:30 +0000

    datatypes

---
+++ 01_basics/01_variables.js
@@ -0,0 +1,11 @@
+const accountId = 2626
+let accountEmail = "Tanishk@gmail.com"
+var accountPassword = "12345"
+accountCity = "Mumbai"
+
+// accountId = 2
+let name;
+//undefined
+console.log(accountId);
+
+console.table([accountEmail, accountPassword, accountId, name, accountCity]);
