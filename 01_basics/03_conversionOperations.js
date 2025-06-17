ccda6bc31e149f3ee2824571565575b44e8b695f
tanish-ashu <ashu2005rajak@gmail.com>
2025-06-17 06:56:24 +0000

    Datatype Conversions

---
+++ 01_basics/03_conversionOperations.js
@@ -0,0 +1,17 @@
+let score = 1.111111191111111911111111111111111111111111111111111111111111191111111;
+
+// console.log(typeof(score));
+
+let valueInNumber = Number(score)
+// console.log(typeof(valueInNumber));
+// console.log(valueInNumber);
+
+console.log(+true)
+console.log(+"")
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100;
+
