f9cb8ce680470760c621d20b3b12065462a33a40
tanish-ashu <ashu2005rajak@gmail.com>
2025-06-17 19:32:45 +0000

    nums and math concepts

---
+++ 01_basics/06_nums_and_math.js
@@ -0,0 +1,36 @@
+const score = 400;
+
+const balance = new Number(100)
+
+// console.log(score);
+// console.log(balance);
+
+// console.log(balance.toString().length);
+// console.log(balance.toFixed(2));
+
+
+const otherNumber = 123.8436
+// console.log(otherNumber.toPrecision(3));
+
+const hundreds = 1000000;
+
+// console.log(hundreds.toLocaleString('en-IN'));
+
+// ======================= MATHS ==============================
+
+// console.log(Math);
+// console.log(Math.abs(-4));
+// console.log(Math.round(4.6));
+// console.log(Math.ceil(4.6));
+// console.log(Math.floor(4.6));
+// console.log(Math.min(3,4,5,8,8));
+// console.log(Math.max(3,4,5,8,8));
+
+console.log(Math.random());  // always btw 0 and 1
+console.log((Math.random() * 10) + 1);  // 
+console.log((Math.floor(Math.random() * 10) + 1));  // 
+
+const min = 10;
+const max = 20;
+
+console.log(Math.floor(Math.random() * (max - min + 1) + min)); // important ** ** **
