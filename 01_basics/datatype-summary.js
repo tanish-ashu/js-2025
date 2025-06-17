d1c71a96d7972393968ec5c0a981debd47a52714
tanish-ashu <ashu2005rajak@gmail.com>
2025-06-17 07:33:03 +0000

    Stack and heap Memory

--- 01_basics/datatype-summary.js
+++ 01_basics/datatype-summary.js
@@ -8,10 +8,10 @@ const score = 100;
 const id = Symbol('123');
 const anotherId = Symbol('123');
 
-console.log(id === anotherId);
+//console.log(id === anotherId);
 
 const bigInt = 34634635n;
-console.log(typeof(bigInt));
+//console.log(typeof(bigInt));
 
 
 // Non- Primitive or Reference 
@@ -26,12 +26,39 @@ let myObj = {
     age: 20
 };
 
-console.log(names);
-console.log(myObj);
+//console.log(names);
+//console.log(myObj);
 
 
-const myFunction = function(){
-    console.log("this is a sample function");
+const myFunction = function(){ 
+    //console.log("this is a sample function");
 }
 
-console.log(typeof( myFunction));
+//console.log(typeof( myFunction));
+
+
+// ++++++++++++++++++++++++++++++++++++++++++
+
+// Stack (Primitive) , Heap (Non-Primitive)
+
+let myName = "Tanishk Rajak";
+
+let anotherName  = myName;
+
+anotherName = "Ashu"
+
+console.log(anotherName);
+console.log(myName);
+
+let user1 = {
+    email: "ashu2005rajak@gmail.com",
+    upi: "8817786102@ybl"
+};
+
+let user2 = user1;
+
+console.log(user1,"\n" ,user2);
+
+user2.email = "do not have email";
+
+console.log(user1.email, "\n" ,user2.email);
