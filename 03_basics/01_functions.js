5177b07cb553f05ebe431cc52bb78933611c216a
tanish-ashu <ashu2005rajak@gmail.com>
2025-06-18 19:10:54 +0000

    function part 2

--- 03_basics/01_functions.js
+++ 03_basics/01_functions.js
@@ -22,5 +22,34 @@ function loginUserMessage(username = "root"){
     return `${username} just loggned in`;
 }
 
-console.log(loginUserMessage("Tanishk")); // when nothing will be passed , it will be undefined
-console.log(loginUserMessage()); 
+// console.log(loginUserMessage("Tanishk")); // when nothing will be passed , it will be undefined
+// console.log(loginUserMessage()); 
+
+
+// ++++++++++++++++++++++++++++++++++++++
+
+function calculateCartPrice(val1, val2, ...num1){ // '...' called rest and spread operator on different usecase (here rest operator)
+    return num1;
+}
+
+console.log(calculateCartPrice(200, 400, 500, 600));
+
+const user = {
+    username: "tanish-ashu",
+    id: 26
+}
+
+function handelObject(anyObject){
+    console.log(`Usermae is ${anyObject.username} and id is ${anyObject.id}`)
+}
+
+handelObject(user);
+
+function printArray(getArray){
+    return getArray[1];
+}
+
+console.log(printArray([2,3,5,5]))
+
+
+
