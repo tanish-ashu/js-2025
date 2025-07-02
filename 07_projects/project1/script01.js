7ccc3fecf8887e3a873b55c1ffd1d6a6eda2ca91
tanish-ashu <ashu2005rajak@gmail.com>
2025-07-02 20:00:49 +0000

    color change background js

---
+++ 07_projects/project1/script01.js
@@ -0,0 +1,27 @@
+const buttons = document.querySelectorAll('.button');
+// above obtainer nodelist of buttons
+console.log(buttons);
+const body = document.querySelector("body");
+// got the element body
+console.log(body);
+
+// applying function on each node list element i.e button
+buttons.forEach(function(button){
+  console.log(button);
+  button.addEventListener('click', function(e){
+    console.log (e);
+    console.log(e.target);
+    if(e.target.id === 'grey'){
+      body.style.backgroundColor = e.target.id;
+    }
+    if(e.target.id === 'white'){
+      body.style.backgroundColor = e.target.id;
+    }
+    if(e.target.id === 'blue'){
+      body.style.backgroundColor = e.target.id;
+    }
+    if(e.target.id === 'yellow'){
+      body.style.backgroundColor = e.target.id;
+    }
+  })
+});
